627ac4c24105a17ff4523c833123c7552137365f
zhengqiangzheng <2466877980@qq.com>
2019-12-23 22:06:35 +0800

    first commit

---
+++ csharp实例/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp实例
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
